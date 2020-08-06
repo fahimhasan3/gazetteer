5e55b0f2d53077c574cf44516ff9f9cdb121117e
fahimhasan3 <60883810+fahimhasan3@users.noreply.github.com>
2020-08-06 16:06:09 +0100

    responsive css

--- index.php
+++ index.php
@@ -5,41 +5,45 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gazetteer</title>
-    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"
-        integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
-        crossorigin="" />
-    <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
-        integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
-        crossorigin=""></script>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css" integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ==" crossorigin="" />
+    <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js" integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew==" crossorigin=""></script>
     <script src="javascript/jquery-3.5.1.min.js"></script>
     <script src="javascript/index.js"></script>
     <link rel="stylesheet" type="text/css" href="css/normalize.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="css/index.css">
 </head>
 
 <body>
     <div id="loading"></div>
     <nav>
-        <input type="text" name="searchbar" id="searchBar" placeholder="city, country" autocomplete="on" required />
-        <input type="submit" id="searchButton" name="searchButton" value="Search" />
         <ul>
+            <li>
+                <input type="text" name="searchbar" id="searchBar" placeholder="city, country" autocomplete="on" required />
+            </li>
+            <li>
+                <input type="submit" id="searchButton" name="searchButton" value="Search" class='btn btn-primary' />
+            </li>
             <li class='active' id='showGeneralInfo'>General Info</li>
             <li id='showWeather'>Weather</li>
             <li id='showCurrency'>Currency</li>
         </ul>
+
     </nav>
     <div id="map">
 
     </div>
     <footer>
-        <h1 id='cityNameTitle'></h1>
-        <h2></h2>
+        <section id='cityNameSection'>
+            <h1 id='cityNameTitle'></h1>
+            <h2></h2>
+        </section>
+
         <section id="content">
 
             <section id='general-info'></section>
             <section id="weather">
-                <p>Today: <span id="temperature"></span> C° <img src="images/sunny.png" alt="weather icon"
-                        id='weatherIcon' /></p>
+                <p>Today: <span id="temperature"></span> C° <img src="images/sunny.png" alt="weather icon" id='weatherIcon' /></p>
                 <p id='sky'></p>
                 <table id='weatherHistory'>
                     <tr id='dateRow'>
