e2d827ba31816054efcfd7aba31d4943a017075f
FAHIM-DESKTOP\fahim <fahimgasan@gmail.com>
2025-05-08 14:11:35 +0100

    more config fix

---
+++ server-php/api-config.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'open_exchange_rates_api_key' => getenv('open_exchange_rates_api_key'),
+    'open_weather_api_key' => getenv('open_weather_api_key'),
+    'geocode_api_key' => getenv('geocode_api_key'),
+    'geonames_username' => getenv('geonames_username'),
+];
